45c4e6cd9a72677efd2ab2d2062982910acfdd5b
Noah Krieger <noahkrieger@gmail.com>
2025-05-10 11:20:49 -0400

    updated config to use hostname

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 
 class Settings(BaseSettings):
-    MONGODB_URI: str = Field(default="mongodb://localhost:27017", env="MONGODB_URI")
+    MONGODB_URI: str = Field(default="mongodb://globeco-security-service-mongodb:27017", env="MONGODB_URI")
     MONGODB_DB: str = Field(default="securities", env="MONGODB_DB")  # Database for GlobeCo Security Service
 
 settings = Settings()
