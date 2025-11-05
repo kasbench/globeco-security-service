5ea59b8193ec44e47eda1bebcd0b44edaeb3168f
Noah Krieger <noahkrieger@gmail.com>
2025-11-05 08:33:37 -0500

    froze docker tages

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from globeco-security-service!")
-
-
-if __name__ == "__main__":
-    main()
--- kbuild.sh
+++ kbuild.sh
@@ -1,3 +1,6 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t kasbench/globeco-security-service:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 \
+-t kasbench/globeco-security-service:latest \
+-t kasbench/globeco-security-service:1.0.0 \
+--push .
 kubectl delete -f k8s/deployment.yaml
 kubectl apply -f k8s/deployment.yaml
